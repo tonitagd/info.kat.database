2d9cd253f17e6dc111008ed5be3e9ceb40ff7d23
Toni Ilieva <toni.krasimirova@gmail.com>
2013-04-29 20:50:29 +0300

    database added

--- KatDatabaseInfo/KatDatabaseInfo/Properties/Settings.Designer.cs
+++ KatDatabaseInfo/KatDatabaseInfo/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace KatDatabaseInfo.Properties {
                 return ((string)(this["KatDatabaseConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\Data\\Database.mdf;In" +
+            "tegrated Security=True")]
+        public string DatabaseConnectionString {
+            get {
+                return ((string)(this["DatabaseConnectionString"]));
+            }
+        }
     }
 }

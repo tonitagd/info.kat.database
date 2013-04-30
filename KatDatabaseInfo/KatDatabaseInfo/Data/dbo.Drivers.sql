601ce30592ba3cb35477362d91cf6c7485522442
Toni Ilieva <toni.krasimirova@gmail.com>
2013-04-30 13:30:02 +0300

    blabla

---
+++ KatDatabaseInfo/KatDatabaseInfo/Data/dbo.Drivers.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Drivers] (
+    [Id]                   INT           NOT NULL,
+    [FirstName]            NVARCHAR (50) NOT NULL,
+    [MiddleName]           NVARCHAR (50) NOT NULL,
+    [LastName]             NVARCHAR (50) NOT NULL,
+    [IdNumber]             NCHAR (10)    NOT NULL,
+    [BirthDate]            NVARCHAR (50) NOT NULL,
+    [Gender]               NCHAR (10)    NOT NULL,
+    [Address]              NVARCHAR (50) NOT NULL,
+    [Country]              NVARCHAR (50) NOT NULL,
+    [City]                 NVARCHAR (50) NOT NULL,
+    [DrivingLicenseNumber] NCHAR (10)    NOT NULL,
+    [DrivingCategories]    NVARCHAR (50) NOT NULL,
+    [DrivingPointsLeft]    SMALLINT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ KatDatabaseInfo/KatDatabaseInfo/Data/dbo.Vehicle.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Vehicle] (
+    [Id]                 INT           NOT NULL,
+    [RegistryNumber]     NVARCHAR (10) NOT NULL,
+    [FrameNumber]        NVARCHAR (20) NOT NULL,
+    [EngineNumber]       NVARCHAR (10) NOT NULL,
+    [Brand]              NVARCHAR (50) NOT NULL,
+    [Model]              NVARCHAR (50) NOT NULL,
+    [Type]               NVARCHAR (50) NOT NULL,
+    [Seats]              SMALLINT      NOT NULL,
+    [Weight]             INT           NOT NULL,
+    [Color]              NVARCHAR (50) NOT NULL,
+    [RegistryDate]       NVARCHAR (50) NOT NULL,
+    [OwnerLicenseNumber] NCHAR (10)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

8b80673e45964b4ebad21abed0c0a914916f536b
Toni Ilieva <toni.krasimirova@gmail.com>
2013-04-30 13:16:59 +0300

    all tables added

---
+++ KatDatabaseInfo/KatDatabaseInfo/Data/dbo.Fines.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Fines]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Type] NVARCHAR(50) NOT NULL, 
+    [SerialNumber] NVARCHAR(50) NOT NULL, 
+    [Date] NVARCHAR(50) NOT NULL, 
+    [Policeman] NVARCHAR(50) NOT NULL, 
+    [OffenderDLN] NVARCHAR(50) NOT NULL, 
+    [Paid] TINYINT NOT NULL
+)

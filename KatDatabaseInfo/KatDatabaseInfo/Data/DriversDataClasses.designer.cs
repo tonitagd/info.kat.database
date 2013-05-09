783be79c94cc20b1e9b2ff015f9b09e7f5a63c9c
Toni Ilieva <toni.krasimirova@gmail.com>
2013-05-09 16:48:25 +0300

    asdasdsa

--- KatDatabaseInfo/KatDatabaseInfo/Data/DriversDataClasses.designer.cs
+++ KatDatabaseInfo/KatDatabaseInfo/Data/DriversDataClasses.designer.cs
@@ -92,7 +92,7 @@ namespace KatDatabaseInfo.Data
 		
 		private string _BirthDate;
 		
-		private string _Gender;
+		private int _Gender;
 		
 		private string _Address;
 		
@@ -122,7 +122,7 @@ namespace KatDatabaseInfo.Data
     partial void OnIdNumberChanged();
     partial void OnBirthDateChanging(string value);
     partial void OnBirthDateChanged();
-    partial void OnGenderChanging(string value);
+    partial void OnGenderChanging(int value);
     partial void OnGenderChanged();
     partial void OnAddressChanging(string value);
     partial void OnAddressChanged();
@@ -263,8 +263,8 @@ namespace KatDatabaseInfo.Data
 			}
 		}
 		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Gender", DbType="NChar(10) NOT NULL", CanBeNull=false)]
-		public string Gender
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Gender", DbType="Int NOT NULL")]
+		public int Gender
 		{
 			get
 			{
---
+++ KatDatabaseInfo/KatDatabaseInfo/Logic/DriverValidation.cs
@@ -0,0 +1,19 @@
+﻿using KatDatabaseInfo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatDatabaseInfo.Logic
+{
+    class DriverValidation
+    {
+
+        public static Driver IsThereDriver(User usr)
+        {
+            return UserData.GetDriverByLicenseID(usr.DrivingLicenseN);
+        }
+
+    }
+}
